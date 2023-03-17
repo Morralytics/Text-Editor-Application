4d8b8677afdda60b4c76eed159f748bbff6cc4d8
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-17 12:17:41 -0700

    Returns all data from the database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -21,7 +21,14 @@ export const putDb = async (id, content) => {
   console.log('Data saved to the database!', result)
 }
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Result value', result);
+  return result;
+};
 
 initdb();

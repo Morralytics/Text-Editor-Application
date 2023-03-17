9d3c7573b9e874e2bbe4921aa9b7704c6dcd60dd
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-17 11:46:37 -0700

    Finished webpack pwa manifest.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,7 +39,11 @@ module.exports = () => {
         start_url: '/',
         publicPath: './',
         icons: [
-
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
         ],
       }),
     ],
